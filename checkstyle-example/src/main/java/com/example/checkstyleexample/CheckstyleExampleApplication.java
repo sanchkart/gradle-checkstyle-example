2187bd26b5803553906e08543ce35252e6b11895
Sanchir Kartiev <sanchir.kartiev@gmail.com>
2022-06-08 12:06:50 +0200

    Init

---
+++ checkstyle-example/src/main/java/com/example/checkstyleexample/CheckstyleExampleApplication.java
@@ -0,0 +1,11 @@
+package com.example.checkstyleexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckstyleExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CheckstyleExampleApplication.class, args);
+    }
+}
---
+++ checkstyle-example/src/test/java/com/example/checkstyleexample/CheckstyleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkstyleexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckstyleExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
